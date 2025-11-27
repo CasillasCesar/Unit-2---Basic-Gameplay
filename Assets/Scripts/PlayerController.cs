c5a3ad45a6e1bc791e913ff207df5d1a18317b3e
CasillasCesar <78441226+CasillasCesar@users.noreply.github.com>
2025-11-27 02:01:24 -0600

    Keep the player inbounds

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
     private const float speed = 10.0f;
+    private const int limit = -10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < limit)
+        {
+            transform.position = new Vector3(limit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > -limit)
+        {
+            transform.position = new Vector3(-limit, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.left * horizontalInput * Time.deltaTime * speed);
     }
